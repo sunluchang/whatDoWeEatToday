a979ecb6b4cf961612f7e3060e0ea13a9ab3d86c
Sun <sunluchang@live.com>
2016-05-17 11:31:14 +0800

    添加项目文件。

---
+++ whatDoWeEatToday/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace whatDoWeEatToday
+{
+    partial class 今天吃什么
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(今天吃什么));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fooddd = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(259, 60);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "日期";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(42, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SHOW_CUR_DATE";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(227)))), ((int)(((byte)(18)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微软雅黑 Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(12, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "今天吃什么";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fooddd
+            // 
+            this.fooddd.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.fooddd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(227)))), ((int)(((byte)(18)))));
+            this.fooddd.Location = new System.Drawing.Point(13, 123);
+            this.fooddd.Name = "fooddd";
+            this.fooddd.Size = new System.Drawing.Size(259, 45);
+            this.fooddd.TabIndex = 2;
+            this.fooddd.Text = " ";
+            this.fooddd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("微软雅黑 Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(12, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = " ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // 今天吃什么
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(284, 139);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fooddd);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "今天吃什么";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "今天吃什么？";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label fooddd;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ whatDoWeEatToday/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace whatDoWeEatToday
+{
+    public partial class 今天吃什么 : Form
+    {
+        string date;
+        string[] md = new string[16];
+        string[] food = new string[] { "黄焖鸡米饭", "九旦拉面", "海南鸡饭", "包子", "麻辣香锅", "麻辣烫", "约泡", "炒饭", "炒面", "叫外卖吧", "辣婆婆", "石锅拌饭", "自己想办法" };
+        string[] foodE = new string[] { "HuangMJ", "JiuDanLM", "HaiNanJF", "BaoZi", "MaLaZM", "MaLaTang", "YuePao", "ChaoFan", "ChaoMian", "BaiduWM", "LaPoPo", "ShiGuoBanFan", "YOURSELF" };
+        public 今天吃什么()
+        {
+            InitializeComponent();
+            date = System.DateTime.Now.ToLongDateString().ToString();
+            label1.Text = date;
+            int DateX = (300 - label1.Text.Length * 18) / 2;
+            int DateY = 20;
+            label1.Location = new System.Drawing.Point(DateX, DateY);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Size = new System.Drawing.Size(300, 235);
+            Random sel = new Random();
+            int i = sel.Next(food.Length);
+
+            fooddd.Text = food[i];
+            label2.Text = foodE[i];
+
+            fooddd.Location = new System.Drawing.Point(10, 120);
+            label2.Location = new System.Drawing.Point(10, 155);
+            this.Text = "今天吃什么？" + "-" + food[i];
+        }
+    }
+}
